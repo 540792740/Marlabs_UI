b5f3679b76318870e13d4dacc777346dd696765f
peter <peterwangjiawei@gmail.com>
2020-02-13 15:17:10 -0500

    test

---
+++ src/Java_basic_random.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.Random;
+
+public class Java_basic_random {
+    public static void main(String[] args){
+        Random r = new Random();
+        for(int i = 0; i < 3; i++){
+            int number = r.nextInt(10);
+            System.out.println(number);
+        }
+    }
+}
