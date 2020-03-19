a454c9a237c4996476cac74b53743ae186fb38b2
Jiawei Wang <31707135+540792740@users.noreply.github.com>
2020-03-19 09:21:17 -0400

    Add files via upload
    MAr 18

---
+++ src/Marlabs_Course/Mar_18_var.ts
@@ -0,0 +1,20 @@
+var age : number = 33;
+// @ts-ignore
+var name : string = "John"
+var isTrue : boolean = true;
+
+var student:{
+    id:number;
+    name:string;
+};
+
+var st = {
+    id : 11,
+    name:"john"
+};
+
+let arr:string[] = ["John", "Adam"];
+
+let a = [1, 2,"hi", true];
+
+let values : (string|number)[] = [ 12, "string"];
