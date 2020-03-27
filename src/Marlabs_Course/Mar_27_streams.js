b7f200ecd3b6458e62ed01690007f90e4851a2e2
peter <peterwangjiawei@gmail.com>
2020-03-27 10:29:28 -0400

    Mar 27 pipes

---
+++ src/Marlabs_Course/Mar_27_streams.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var readableStream  = fs.createReadStream('Mar_27.txt');
+
+var data = "";
+readableStream.setEncoding('UTF8');
+
+readableStream.on('data',function (chunk) {
+    data += chunk;
+    console.log("-------------");
+    console.log(chunk);
+    console.log("-------------");
+});
+readableStream.on('end',function () {
+    console.log("-------------");
+    console.log(data)
+});
+
+
+var data = "Some dummy data to write";
+var writableStream = fs.createReadStream('Mar_27_output.txt');
+writableStream.write(data,'UTF-8');
+writableStream.end();
+writableStream.on('finish', function () {
+    consolo.log("Finish Writing")
+});
