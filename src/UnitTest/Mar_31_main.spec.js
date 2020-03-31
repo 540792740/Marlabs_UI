88934c03f2004c82807690947fe0c09f6fd78016
peter <peterwangjiawei@gmail.com>
2020-03-31 14:51:49 -0400

    Mar 31 main

---
+++ src/UnitTest/Mar_31_main.spec.js
@@ -0,0 +1,16 @@
+describe("Mar_31_main.js", function () {
+    describe('calculate', function(){
+        xit("Validate Expression");
+        xit("Calls add");
+        xit("Calls Substract");
+        xit("Calls multiple");
+        xit("Calls Divide");
+
+    })
+
+});
+
+describe('updateResult()', function () {
+   element = document.createElement('span');
+   // element.
+});
