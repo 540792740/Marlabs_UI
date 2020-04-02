08c46c9a7d3047db131bea93faa34d3e94e8dec7
peter <peterwangjiawei@gmail.com>
2020-04-02 12:37:19 -0400

    Apr 2 firebase

--- src/Angular8/angularfire/src/app/services/item.service.ts
+++ src/Angular8/angularfire/src/app/services/item.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore,
   AngularFirestoreCollection,
+  // tslint:disable-next-line:import-spacing
   AngularFirestoreDocument}
   from 'angularfire2/firestore';
 import {Item} from '../models/Item';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  // itemsCollection: AngularFirestoreCollection<Item>;
+  itemsCollection: AngularFirestoreCollection<Item>;
   // what is this line used for? this file i didnot use
   items: Observable<Item[]>;
 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection('items').valueChanges();
-
+    // this.items = this.afs.collection('items').valueChanges();
+    this.itemsCollection = this.afs.collection('items')
+    this.items = this.itemsCollection.snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    }));
   }
 
   getItems() {
