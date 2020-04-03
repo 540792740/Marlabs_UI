0025c4c80946c02b5c1a825a70587dc6af78e372
peter <peterwangjiawei@gmail.com>
2020-04-03 12:39:08 -0400

    Apr 3 987

--- src/Angular8/Apr3Test/src/app/matchStrubgTest.spec.ts
+++ src/Angular8/Apr3Test/src/app/matchStrubgTest.spec.ts
@@ -8,20 +8,27 @@ describe('String Match test',()=>{
   it('Test String, .not.toBe',()=>{
     let str1 = "Hello World!";
     expect(str1).not.toBe('Hello World');
-  })
+  });
 
   it('Test String, toEqual',()=>{
     let str1 = "Hello World!";
     expect(str1).toEqual('Hello World!');
-  })
+  });
 
   it('Test String, not.toEqual',()=>{
     let str1 = "Hello World!";
     expect(str1).not.toEqual('Hello World');
-  })
+  });
 
   it('Test String, toContain',()=>{
     let str1 = "Hello World!";
     expect(str1).toContain('Hello');
+  });
+
+  it('Test String, toContain with Arrays',()=>{
+    let a = ['a','b','c'];
+    expect(a).toContain('b');
   })
+
+
 });
