ec5063b70b48c79cc5cc1e13901e8ff3ea1832ca
peter <peterwangjiawei@gmail.com>
2020-04-07 12:19:54 -0400

    Apr 7 goodmong

--- src/MEAN/Readme.md
+++ src/MEAN/Readme.md
@@ -2,9 +2,22 @@
 
 
 `Hello`
-1.  ng new project
+1.  `ng new project`
 2.  goto angular.json: outputPath: remove last path.
-3.  ng build(complied project)
-4.  npm install --save express body-parser
+3.  `ng build`(complied project)
+4.  `npm install --save express body-parser`
 5.  create server.js, create server\routes\api.js 
-6.  node server, ng serve
+6.  `node server`, `ng serve`
+
+7.  MongoDB: create employee/video:
+        {
+          "title":"Mr Fundemetals"
+          "url":"https://www.youtube.com/embed/sDee5a0Fu7A"
+          "description":"Video on Interview Research"
+        }  
+8.  `npm install --save mongoose`
+
+9.
+
+10. 
+
--- src/MEAN/ngApp/server/routes/api.js
+++ src/MEAN/ngApp/server/routes/api.js
@@ -1,9 +1,45 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
+const Video = require('../models/video');
+const db = 'mongodb://localhost:27017/Employee';
+mongoose.Promise = global.Promise;
+
+//connection
+mongoose.connect(db, function (err) {
+  if(err){
+    console.log("Error!:" + err);
+  }
+});
+
 
 router.get('/', function (req, res) {
   res.send('api works');
 });
+
+router.get('/videos/:id', function (req, res) {
+  console.log("Get request for all videos")
+  Video.findById(req.params.id)
+    .exec(function (err, result) {
+      if(err) {
+        console.log("Error retrieving Videos")
+      }else{
+        res.json(result);
+      }
+    });
+});
+
+router.get('/videos', function (req, res) {
+  console.log("Get request for all videos")
+  Video.find({})
+    .exec(function (err, result) {
+      if(err) {
+        console.log("Error retrieving Videos")
+      }else{
+        res.json(result);
+      }
+    });
+});
+
 
 module.exports = router;
