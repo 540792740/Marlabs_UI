e1dde415d12b1106649a302be2bc56c8e40a5582
peter <peterwangjiawei@gmail.com>
2020-04-09 14:01:33 -0400

    Apr_9 react98765

--- src/React/react-app/src/Components/Apr_9_Counter.js
+++ src/React/react-app/src/Components/Apr_9_Counter.js
@@ -3,30 +3,42 @@ import React, {Component} from 'react';
 class Apr9Counter extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             count:0
         }
     }
-
     increment() {
-        this.setState({
-            count: this.state.count + 1
-        },
-            () =>{
-                console.log("Callback : " + this.state.count)
-            }
-        );
+        //Example 1
+        // this.setState({
+        //     count: this.state.count + 1
+        // },
+        //     () =>{
+        //         console.log("Callback : " + this.state.count)
+        //     }
+        // );
+
+        //Example 2
+        this.setState(prevState=>({
+            count:prevState.count + 1
+                }), () =>{
+                    console.log("Callback : " + this.state.count)
+                    }
+                );
+
         console.log(this.state.count)
 
     }
-
+    incrementFive(){
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+    }
     render() {
         return (
             <div>
-                <button onClick={() =>this.increment()}>Increment</button>
+                <button onClick={() =>this.incrementFive()}>Increment</button>
                 <div>Count = {this.state.count}</div>
-
             </div>
         );
     }
