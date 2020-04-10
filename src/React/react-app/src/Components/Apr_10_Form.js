849fbb4305c6b300ca70c5834966b78544a08fc4
peter <peterwangjiawei@gmail.com>
2020-04-10 13:21:33 -0400

    apr 10 w987654

--- src/React/react-app/src/Components/Apr_10_Form.js
+++ src/React/react-app/src/Components/Apr_10_Form.js
@@ -4,7 +4,10 @@ class Apr10Form extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            username:'CC'
+            username:'CC',
+            comment: 'Cris',
+            phone:'Google'
+
         }
     }
     handleFormChange=(event)=>{
@@ -13,6 +16,24 @@ class Apr10Form extends Component {
         });
         console.log(event.target.value)
     };
+
+    handleCommonChange =(event)=>{
+        this.setState ({
+            comment:event.target.value
+        }, ()=>{
+            console.log(this.state.comment)
+
+        });
+
+    };
+    handlePhoneChange =(event)=> {
+        this.setState({
+            phone: event.target.value
+        }, () => {
+            console.log(this.state.phone)
+
+        });
+    }
     render() {
         return (
             <form>
@@ -23,9 +44,21 @@ class Apr10Form extends Component {
                     />
                 </div>
                 <div>
-
+                    <label>Comments</label>
+                    <textarea value={this.state.comment} onChange={this.handleCommonChange} cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label>Phones</label>
+                    <select value={this.state.phone} onChange={this.handlePhoneChange}>
+                        <option value="Sumsung">Samngsung</option>
+                        <option value="LG">LG</option>
+                        <option value="Google">Google</option>
+                        <option value="Facebook">Facebook</option>
+                    </select>
 
                 </div>
+
+
             </form>
             );
     }
