b2d9af05d075ff77994768cdea691899edeab9c8
peter <peterwangjiawei@gmail.com>
2020-04-10 12:47:53 -0400

    apr 10 987

--- src/React/react-app/src/Components/Apr_10_inlineStyle.js
+++ src/React/react-app/src/Components/Apr_10_inlineStyle.js
@@ -1,13 +1,18 @@
 import React from 'react';
+// import styles from "./Apr_10_appStyle.module.css";
 
 const heading={
-    fontSize: '75px',
+    fontSize: '15px',
     color:'blue'
 }
 function Apr10InlineStyle(props) {
     return (
         <div>
             <h3 style={heading}>Inline</h3>
+            <h1 className="error">Error</h1>
+
+            {/*Child module does not work, need to be imported */}
+            {/*<h1 className={styles.success}>Success</h1>*/}
         </div>
     );
 }
