b2abb7a877f0b5302bfcb87002de252737cd2f97
peter <peterwangjiawei@gmail.com>
2020-04-10 13:01:39 -0400

    apr 10 9

--- src/React/react-app/src/Components/Apr_10_Form.js
+++ src/React/react-app/src/Components/Apr_10_Form.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 
 class Apr10Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            username:'CC'
+        }
+    }
+    handleFormChange=(event)=>{
+        this.setState ({
+            username:event.target.value
+        })
+    };
+
     render() {
         return (
             <form>
                 <label>Username : </label>
-                <input type="text"/>
+                <input type="text" value={this.state.username}
+                onChange={this.handleFormChange}
+                />
             </form>
             );
     }
