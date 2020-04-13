e8256895468c3dbf590562ba727b7f67243ece80
peter <peterwangjiawei@gmail.com>
2020-04-13 11:33:17 -0400

    Apr 13 9

--- src/React/react-app/src/Components/Apr_13_RefsDemo.js
+++ src/React/react-app/src/Components/Apr_13_RefsDemo.js
@@ -4,18 +4,35 @@ class Apr13RefsDemo extends Component {
     constructor(props) {
         super(props);
         this.inputRef = React.createRef();
+        this.cdRef = null;
+        this.setCbRef = (element)=> {
+            this.cdRef = element;
+        }
     }
 
     componentDidMount() {
-        console.log(this.inputRef)
-
+        // console.log(this.inputRef)
+        this.inputRef.current.focus();
+        if(this.cdRef){
+            this.cdRef.value = "CD Ref"
+            this.cdRef.select();
+        }
     }
 
+    clickHandler= ()=>{
+        alert(this.inputRef.current.value)
+    };
 
+    handleFocus = ()=>{
+
+    }
     render() {
         return (
             <div>
                 <input type="text" ref={this.inputRef}/>
+                <input type="text" ref={this.setCbRef}/>
+                <input type="text" ref={this.setCbRef} onFocus={this.handleFocus}/>
+                <button onClick={this.clickHandler}>Click</button>
             </div>
         );
     }
