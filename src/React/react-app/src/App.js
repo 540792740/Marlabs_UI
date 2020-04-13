5a109561bea4c3c5bc9a5de4be7d941f30dcdf88
peter <peterwangjiawei@gmail.com>
2020-04-13 11:48:55 -0400

    Apr 13 298765432

--- src/React/react-app/src/App.js
+++ src/React/react-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //Day 1
 import './Components/Apr_10_appStyle.css'
 import Apr13RefsDemo from "./Components/Apr_13_RefsDemo";
+import Apr13ClickCounter from "./Components/Apr_13_ClickCounter";
 
 
 class App extends Component{
@@ -19,44 +20,46 @@ class App extends Component{
     render(){
         return (
           <div className="App">
-          {/*Day 1  Apr 9 */}
-          {/*<Hello/><hr/>*/}
-          {/*<Apr_9_Greet name="React" dev="Robin" >*/}
-          {/*    <h3>Child Element</h3>*/}
-          {/*    <input type="button" value="click"/>*/}
-          {/*</Apr_9_Greet><hr/>*/}
-          {/*<Apr_9_Greet name="Angular" dev="Cris"></Apr_9_Greet>*/}
-          {/*<Apr_9_Greet name="Node" dev="Baby"></Apr_9_Greet>*/}
-          {/*<Apr_9_Greet name="Jquery" dev="G.E.M"></Apr_9_Greet><hr/>*/}
-          {/*<Apr_9_Welcome name="Angular" dev="Cris"></Apr_9_Welcome>*/}
-          {/*<Apr_9_Welcome name="Angular" dev="Cris"></Apr_9_Welcome>*/}
-          {/*<Apr_9_Welcome name="Node" dev="Baby"></Apr_9_Welcome>*/}
-          {/*<Apr_9_Welcome name="Node" dev="Baby"></Apr_9_Welcome><hr/>*/}
-          {/*<Apr_9_Message></Apr_9_Message> <hr/>*/}
-          {/*<Apr9Counter></Apr9Counter>       <hr/>*/}
-          {/*<Apr9Event></Apr9Event> <hr/>*/}
-          {/*<Apr9EventBinding></Apr9EventBinding>*/}
+              {/*Day 1  Apr 9 */}
+              {/*<Hello/><hr/>*/}
+              {/*<Apr_9_Greet name="React" dev="Robin" >*/}
+              {/*    <h3>Child Element</h3>*/}
+              {/*    <input type="button" value="click"/>*/}
+              {/*</Apr_9_Greet><hr/>*/}
+              {/*<Apr_9_Greet name="Angular" dev="Cris"></Apr_9_Greet>*/}
+              {/*<Apr_9_Greet name="Node" dev="Baby"></Apr_9_Greet>*/}
+              {/*<Apr_9_Greet name="Jquery" dev="G.E.M"></Apr_9_Greet><hr/>*/}
+              {/*<Apr_9_Welcome name="Angular" dev="Cris"></Apr_9_Welcome>*/}
+              {/*<Apr_9_Welcome name="Angular" dev="Cris"></Apr_9_Welcome>*/}
+              {/*<Apr_9_Welcome name="Node" dev="Baby"></Apr_9_Welcome>*/}
+              {/*<Apr_9_Welcome name="Node" dev="Baby"></Apr_9_Welcome><hr/>*/}
+              {/*<Apr_9_Message></Apr_9_Message> <hr/>*/}
+              {/*<Apr9Counter></Apr9Counter>       <hr/>*/}
+              {/*<Apr9Event></Apr9Event> <hr/>*/}
+              {/*<Apr9EventBinding></Apr9EventBinding>*/}
 
-          {/*    Video*/}
-          {/*<ReactVideo></ReactVideo><hr/>*/}
-          {/*<Apr9State></Apr9State><hr/>*/}
-          {/*<Apr9StateTest></Apr9StateTest><hr/>*/}
-          {/*<Apr9CartSample></Apr9CartSample><hr/>*/}
-          {/*{this.state.prop && <Apr12LifeCycle prop={this.state}></Apr12LifeCycle>}*/}
+              {/*    Video*/}
+              {/*<ReactVideo></ReactVideo><hr/>*/}
+              {/*<Apr9State></Apr9State><hr/>*/}
+              {/*<Apr9StateTest></Apr9StateTest><hr/>*/}
+              {/*<Apr9CartSample></Apr9CartSample><hr/>*/}
+              {/*{this.state.prop && <Apr12LifeCycle prop={this.state}></Apr12LifeCycle>}*/}
 
-          {/*    Day 2 Apr 10*/}
-          {/*<Apr10ParentComponent></Apr10ParentComponent><hr/>*/}
-          {/*<Apr10CondRen></Apr10CondRen><hr/>*/}
-          {/*<Apr10ListRen></Apr10ListRen><hr/>*/}
-          {/*<Apr10StyleSheet primary={true}></Apr10StyleSheet><hr/>*/}
-          {/*<Apr10InlineStyle></Apr10InlineStyle><hr/>*/}
-          {/*<Apr10Form></Apr10Form><hr/>*/}
-          {/*<Apr10LifeCycleParent></Apr10LifeCycleParent>*/}
+              {/*    Day 2 Apr 10*/}
+              {/*<Apr10ParentComponent></Apr10ParentComponent><hr/>*/}
+              {/*<Apr10CondRen></Apr10CondRen><hr/>*/}
+              {/*<Apr10ListRen></Apr10ListRen><hr/>*/}
+              {/*<Apr10StyleSheet primary={true}></Apr10StyleSheet><hr/>*/}
+              {/*<Apr10InlineStyle></Apr10InlineStyle><hr/>*/}
+              {/*<Apr10Form></Apr10Form><hr/>*/}
+              {/*<Apr10LifeCycleParent></Apr10LifeCycleParent>*/}
 
-          {/*Day 3 Apr 13*/}
-          {/*<h3>home</h3>*/}
-          {/*<Apr13ParCom></Apr13ParCom>*/}
-          <Apr13RefsDemo></Apr13RefsDemo>
+              {/*Day 3 Apr 13*/}
+              {/*<h3>home</h3>*/}
+              {/*<Apr13ParCom></Apr13ParCom>*/}
+              <Apr13RefsDemo></Apr13RefsDemo>
+              <Apr13ClickCounter></Apr13ClickCounter>
+
         </div>
         )
     }
--- src/React/react-app/src/Components/Apr_13_ClickCounter.js
+++ src/React/react-app/src/Components/Apr_13_ClickCounter.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 
 class Apr13ClickCounter extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            count:0
+        }
+
+    }
+    incrementCount = () => {
+        this.setState(prevState=>{
+            return{count:prevState.count + 1}
+        })
+    };
+
     render() {
         return (
             <div>
-                
+
+                <button onClick={this.incrementCount}> Clicked {this.state.count}</button>
             </div>
         );
     }
