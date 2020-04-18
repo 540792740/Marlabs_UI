4c340062a9f117262f2c873bb532ee238e0ddad3
peter <peterwangjiawei@gmail.com>
2020-04-17 22:13:25 -0400

    Apr 17 12987675

--- src/React/react-app/src/Apr_9_ReactVideo/Apr_16_Composition.js
+++ src/React/react-app/src/Apr_9_ReactVideo/Apr_16_Composition.js
@@ -14,19 +14,90 @@ function Dialog(props){
 }
 
 //Welcome Dialog provide content
-function WelcomeDialog(props){
+// function WelcomeDialog(props){
+//     return(
+//         <Dialog {...props}>
+//             <h3>Welcome</h3>
+//             <p>Thanks for your using</p>
+//         </Dialog>
+//     )
+// }
+
+// const Api = {
+//     getUser(){
+//         return {name:'Jerry', age:20};
+//     }
+// };
+
+// function Fetcher(props){
+//     const user = Api[props.name]();
+//     return props.children(user)
+// }
+//
+// function Filter({children, type}) {
+//     return(
+//         <div>
+//            { React.Children.map(children, child=>{
+//                 if (child.type !== type){
+//                     return;
+//                 }
+//                 return child
+//         })}
+//         </div>
+//     )
+// }
+
+//Change children using RadioGroup
+function RadioGroup(props){
+    return <div>
+        {React.Children.map(props.children, child=>{
+            child.props.name = props.name;
+        })}
+    </div>
+}
+
+function Radio(props) {
     return(
-        <Dialog {...props}>
-            <h3>Welcome</h3>
-            <p>Thanks for your using</p>
-        </Dialog>
+        <label type="radio" {...props}></label>
     )
 }
 
 function Apr16Composition() {
     return (
-        <WelcomeDialog color="green"></WelcomeDialog>
-    );
+        // <WelcomeDialog color="green"></WelcomeDialog>
+
+        //Example 1
+        // <Fetcher name="getUser">
+        //     {({name, age})=>(
+        //         <p>
+        //             {name} - {age}
+        //         </p>
+        //     )}
+        // </Fetcher>
+
+        //Example 2 Filter the tag
+        // <div>
+        //     <Filter type="p">
+        //         <h3>React</h3>
+        //         <p>React is good</p>
+        //         <h3>Vue</h3>
+        //         <p>Vue is good</p>
+        //     </Filter>
+        //
+        // </div>
+
+        //Example 3 RadioGroup
+        <div>
+            <RadioGroup name="mvvm">
+                <Radio value="Vue">Vue</Radio>
+                <Radio value="React">React</Radio>
+                <Radio value="Angular">Angular</Radio>
+            </RadioGroup>
+        </div>
+
+
+
+    )
 }
 
 export default Apr16Composition;
