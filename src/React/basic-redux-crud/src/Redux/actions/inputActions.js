72f0dfae6847d2ca78b4cd0486c147955d8d5dc6
peter <peterwangjiawei@gmail.com>
2020-04-21 12:10:40 -0400

    Apr 21 new new 98876

---
+++ src/React/basic-redux-crud/src/Redux/actions/inputActions.js
@@ -0,0 +1,12 @@
+import actionType from "../actionType";
+
+export default{
+    setInputTitle: title =>({
+        type:actionType.SET_INPUT_TITLE,
+        title
+    }),
+    setInputContent: content =>({
+        type:actionType.SET_INPUT_CONTENT,
+        content
+    })
+}
