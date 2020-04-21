c07f179ea6903de29f89bd4c7e68616df1de3b59
peter <peterwangjiawei@gmail.com>
2020-04-21 11:52:12 -0400

    Apr 21 new new 98876543

--- src/React/basic-redux-crud/src/Components/NoteSection.js
+++ src/React/basic-redux-crud/src/Components/NoteSection.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './NoteSection.style.scss'
 import NoteItem from './NoteItem'
+import {useSelector} from "react-redux";
 
 function NoteSection(props) {
+    const notes = useSelector ( state => state.notes.notes);
     return (
         <div className="NoteSection_container">
-            <NoteItem title="note Title" content= "Note Content"></NoteItem>
+            {notes.map((item, index) =>(
+                <NoteItem
+                    title={item.title}
+                    content= {item.content/}>
+            ))}
             <NoteItem title="note Title" content= "Note Content"></NoteItem>
             <NoteItem title="note Title" content= "Note Content"></NoteItem>
             <NoteItem title="note Title" content= "Note Content"></NoteItem>
