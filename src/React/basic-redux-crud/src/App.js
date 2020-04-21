2f9946110046df557cd7b4afa977cfc57b4e7cb7
peter <peterwangjiawei@gmail.com>
2020-04-21 10:37:32 -0400

    Apr 21 new new 99876

--- src/React/basic-redux-crud/src/App.js
+++ src/React/basic-redux-crud/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Provider} from 'react-redux'
+import store from "";
 
 function App() {
   return (
-      <Provider>
+      <Provider store={{store}}>
         <div className="App">
-
+            My Notes
         </div>
       </Provider>
   );
