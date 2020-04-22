6b701790ac103a7c05e51cb72ff2ea130e839494
peter <peterwangjiawei@gmail.com>
2020-04-21 23:27:57 -0400

    Apr 21 new n  987654

--- src/React/react-redux-demo/src/components/Apr_21_reduxTest.js
+++ src/React/react-redux-demo/src/components/Apr_21_reduxTest.js
@@ -5,16 +5,20 @@ import {connect} from 'react-redux'
 const mapStateToProps = state => ({num: state});
 const mapDispatchToProps = {
     add:() =>({type:'add'}),
-    minus:() =>({type:'minus'})
+    minus:() =>({type:'minus'}),
+    asyncAdd:() =>dispatch =>{
+        setTimeout(()=>{dispatch({type:'add'})}, 1500);
+    }
 };
 
-function Apr21ReduxTest({num, add, minus}) {
+function Apr21ReduxTest({num, add, minus, asyncAdd}) {
     return (
         <div>
             <p>{num}</p>
             <div>
                 <button onClick={minus}> - </button>
                 <button onClick={add}> + </button>
+                <button onClick={asyncAdd}> AsyncAdd </button>
             </div>
         </div>
     );
