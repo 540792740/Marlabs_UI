4f676784e6fc03162ef4e00342965b538371befe
peter <peterwangjiawei@gmail.com>
2020-04-22 11:29:34 -0400

    Apr 22 new n

--- src/React/basic-redux-crud/src/Components/NoteItem.js
+++ src/React/basic-redux-crud/src/Components/NoteItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './NoteItem.style.scss'
 
-const NoteItem  =  ({title, content, onItemClick}) => {
+const NoteItem  =  ({title, content, onItemClicked}) => {
     return (
         <div className="NoteItem_container"
              role="button"
-             onClick={onItemClick}
+             onClick={onItemClicked}
         >
             <h3>{title}</h3>
             <p>{content}</p>
